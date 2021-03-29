395db37a349494430fe5e476fe95bab2c13a7f32
Elian Bollue <elian.bollue@student.ucll.be>
2021-03-29 11:37:19 +0200

    Update Speler.java

--- Story1/src/domain/Speler.java
+++ Story1/src/domain/Speler.java
@@ -4,6 +4,14 @@ public class Speler {
     String naam;
 
     public Speler(String naam){
+        setNaam(naam);
+    }
+
+    private void setNaam(String naam){
         this.naam = naam;
+    }
+
+    public String getNaam(){
+        return this.naam;
     }
 }
