f49027e08399446d9d5d507cae90bbdb86e3babd
Elian Bollue <elian.bollue@student.ucll.be>
2021-03-30 12:06:36 +0200

    gerald-elian_story06

--- Story1/src/ui/FiguurApp.java
+++ Story1/src/ui/FiguurApp.java
@@ -12,19 +12,18 @@ public class FiguurApp {
     private ObservableList<String> mogelijkeFiguren;
 
     public FiguurApp(GridPane root) {
-        //mogelijkeFiguren = FXCollections.observableArrayList("Cirkel","Rechthoek");
-        mogelijkeFiguren = FXCollections.observableArrayList("Driehoek");
+        mogelijkeFiguren = FXCollections.observableArrayList("Cirkel","Rechthoek","Driehoek");
         keuzeMenu = new ComboBox(mogelijkeFiguren);
         root.add(keuzeMenu,0,0);
         keuzeMenu.setOnAction(eventKeuze -> {
                     keuzeMenu.setVisible(false);
                     if (keuzeMenu.getValue()!=null) {
-                        /* if (keuzeMenu.getValue().equals("Cirkel")) {
+                        if (keuzeMenu.getValue().equals("Cirkel")) {
                             new CirkelApp(root);
 
                         } else if (keuzeMenu.getValue().equals("Rechthoek")) {
                             new RechthoekApp(root);
-                        } else */ if (keuzeMenu.getValue().equals("Driehoek")) {
+                        } else if (keuzeMenu.getValue().equals("Driehoek")) {
                             new DriehoekApp(root);
 
                         }
